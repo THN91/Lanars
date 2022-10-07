61a3e411e08778d2a39cb5bc5c601f1a5cf8fc5a
Taras Honcharuk <taras.goncharuk22@gmail.com>
2022-10-07 17:19:48 +0300

    first commit

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,43 @@
+import React from "react";
+import './App.css';
+
+function Cards() {
+    return (
+        <div className="container">
+            <div className="card">1</div>
+            <div className="card">2</div>
+            <div className="card">3</div>
+            <div className="card">4</div>
+            <div className="card">5</div>
+            <div className="card">6</div>
+            <div className="card">7</div>
+            <div className="card">8</div>
+            <div className="card">9</div>
+            <div className="card">10</div>
+            <div className="card">1</div>
+            <div className="card">2</div>
+            <div className="card">3</div>
+            <div className="card">4</div>
+            <div className="card">5</div>
+            <div className="card">6</div>
+            <div className="card">7</div>
+            <div className="card">8</div>
+            <div className="card">9</div>
+            <div className="card">10</div>
+            <div className="card">1</div>
+            <div className="card">2</div>
+            <div className="card">3</div>
+            <div className="card">4</div>
+            <div className="card">5</div>
+            <div className="card">6</div>
+            <div className="card">7</div>
+            <div className="card">8</div>
+            <div className="card">9</div>
+            <div className="card">10</div>
+            <div className="card">10</div>
+            <div className="card">10</div>
+        </div>
+    );
+}
+
+export default Cards;
